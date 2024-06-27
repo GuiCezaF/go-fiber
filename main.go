fed88f45495751dac70dda306e28e00d7e9e069e
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-06-27 09:27:56 -0300

    feat: iniciando aplicação

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	app := fiber.New()
+
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		err := c.SendString("API is running...")
+		return err
+})
+
+	app.Listen(":3000")
+}
