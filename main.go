39f2817490cb8b26561b5b0b5c076ee721d4eb92
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-07-08 08:57:28 -0300

    feat: config  database connection

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/GuiCezaF/go-fiber/database"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -8,10 +9,12 @@ func main() {
 
 	app := fiber.New()
 
+	database.ConnectDB()
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		err := c.SendString("API is running...")
 		return err
-})
+	})
 
 	app.Listen(":3000")
 }
